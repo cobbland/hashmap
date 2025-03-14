5965a93523149fd96388a6564ef0809d31474fef
cobbland <jacobdensford@proton.me>
2025-03-14 12:05:51 -0400

    Add (partially functional) set function

--- HashMap.js
+++ HashMap.js
@@ -3,6 +3,16 @@ export default class HashMap {
     constructor(loadFactor = 0.75, capacity = 16) {
         this.loadFactor = loadFactor;
         this.capacity = capacity;
+        this.buckets = [];
+        for (let i = 0; i < this.capacity; i++) {
+            this.buckets[i] = [];
+        }
+    }
+
+    boundsCheck(index) {
+        if (index < 0 || index >= this.capacity) {
+            throw new Error("Trying to access index out of bounds");
+        }
     }
 
     hash(key, seed = 31) {
@@ -11,6 +21,26 @@ export default class HashMap {
             hashCode = (seed * hashCode + key.charCodeAt(i)) % this.capacity;
         }
         return hashCode;
+    }
+
+    set(key, value) {
+        // check loadFactor
+        // if maxed out, grow hashmap
+        const index = this.hash(key);
+        this.boundsCheck(index);
+        let inHere = false;
+        for (let thing in this.buckets[index]) {
+            if (
+                this.buckets[index][thing] &&
+                this.buckets[index][thing].key === key
+            ) {
+                this.buckets[index][thing].value = value;
+                inHere = true;
+            }
+        }
+        if (!inHere) {
+            this.buckets[index].push({key, value});
+        }
     }
 
 }
--- main.js
+++ main.js
@@ -1,24 +1,25 @@
 import HashMap from "./HashMap.js";
 
 const newHashMap = new HashMap();
-const arrOfBuckets = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]];
 const arrOfKeys = [
     "Emma", "Liam", "Olivia", "Noah", "Ava", "William", "Sophia", "James", "Isabella", "Oliver",
-    "Mia", "Benjamin", "Charlotte", "Elijah", "Amelia", "Lucas", "Harper", "Mason", "Evelyn", "Logan",
-    "Abigail", "Alexander", "Emily", "Ethan", "Elizabeth", "Jacob", "Mila", "Michael", "Ella", "Daniel",
-    "Avery", "Henry", "Sofia", "Jackson", "Camila", "Sebastian", "Aria", "Aiden", "Scarlett", "Matthew",
-    "Victoria", "Samuel", "Madison", "David", "Luna", "Joseph", "Grace", "Carter", "Chloe", "Owen",
-    "Penelope", "Wyatt", "Layla", "John", "Riley", "Jack", "Zoey", "Luke", "Nora", "Jayden",
-    "Lily", "Dylan", "Eleanor", "Grayson", "Hannah", "Levi", "Lillian", "Isaac", "Addison", "Gabriel",
-    "Aubrey", "Julian", "Ellie", "Mateo", "Stella", "Anthony", "Natalie", "Jaxon", "Zoe", "Lincoln",
-    "Leah", "Joshua", "Hazel", "Christopher", "Violet", "Andrew", "Aurora", "Theodore", "Savannah", "Caleb",
-    "Audrey", "Ryan", "Brooklyn", "Asher", "Bella", "Nathan", "Claire", "Thomas", "Skylar", "Leo"
+    "Mia", "Benjamin", "Charlotte", "Elijah", "Amelia", "Lucas", // "Harper", "Mason", "Evelyn", "Logan",
+    // "Abigail", "Alexander", "Emily", "Ethan", "Elizabeth", "Jacob", "Mila", "Michael", "Ella", "Daniel",
+    // "Avery", "Henry", "Sofia", "Jackson", "Camila", "Sebastian", "Aria", "Aiden", "Scarlett", "Matthew",
+    // "Victoria", "Samuel", "Madison", "David", "Luna", "Joseph", "Grace", "Carter", "Chloe", "Owen",
+    // "Penelope", "Wyatt", "Layla", "John", "Riley", "Jack", "Zoey", "Luke", "Nora", "Jayden",
+    // "Lily", "Dylan", "Eleanor", "Grayson", "Hannah", "Levi", "Lillian", "Isaac", "Addison", "Gabriel",
+    // "Aubrey", "Julian", "Ellie", "Mateo", "Stella", "Anthony", "Natalie", "Jaxon", "Zoe", "Lincoln",
+    // "Leah", "Joshua", "Hazel", "Christopher", "Violet", "Andrew", "Aurora", "Theodore", "Savannah", "Caleb",
+    // "Audrey", "Ryan", "Brooklyn", "Asher", "Bella", "Nathan", "Claire", "Thomas", "Skylar", "Leo"
 ];
 
 for (let key in arrOfKeys) {
-    arrOfBuckets[newHashMap.hash(arrOfKeys[key])].push(arrOfKeys[key]);
+    newHashMap.set(arrOfKeys[key], "some value");
 }
 
-for (let bucket in arrOfBuckets) {
-    console.log(`Bucket ${bucket}: ${arrOfBuckets[bucket].length}`);
-}
+// console.log(newHashMap.buckets);
+
+newHashMap.set("Elijah", "some new value");
+
+console.log(newHashMap.buckets);
